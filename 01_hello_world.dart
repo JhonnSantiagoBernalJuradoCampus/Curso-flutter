7fca36ed97ed7319b6c77c0a5601acb8aa9006d7
jbern <jhon.bernal@pensemos.com>
2024-08-12 12:03:41 -0500

    feat: :sparkles: Hola mundo con Dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,10 @@
+void main(){
+  // var myName = 'Santiago';
+  // late final myName;
+  // String myName = 'Santiago';
+  const myName = 'Santiago';
+  
+  
+  print('Hola ${myName.toUpperCase()}');
+  print('${1 == 1.toString()}');
+}
